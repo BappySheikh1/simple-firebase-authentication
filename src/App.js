2834cd25f52785d6e4eccc3495924ddc29ed6e1d
Your Name Main <you@example.com>
2022-10-12 23:00:20 -0700

    toggle added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app)
+
 function App() {
+  const [user,setUser]=useState({})
+
   const provider = new GoogleAuthProvider()
 
   const handleGoogleSignIn=()=>{
@@ -11,6 +15,7 @@ function App() {
     .then(result =>{
 
       const user =result.user
+      setUser(user)
       console.log(user)
     })
     .catch(error=>{
@@ -18,10 +23,33 @@ function App() {
     })
   }
 
+  const handleSignOut=()=>{
+//  console.log('handle sign out');
+signOut(auth)
+.then(() =>{
+  setUser({})
+})
+.catch(error =>{
+  console.log(error);
+  setUser({})
+})
+  }
+
   return (
     <div className="App">
+     {/* consition ? true: false */}
+     { user.email ? 
      
+     <button onClick={handleSignOut}>Google Sign Out</button>
+     :
      <button onClick={handleGoogleSignIn}>Google Sing in</button>
+     },
+
+     { user.email && <div>
+      <h3>user name: {user.displayName}</h3>
+      <p>email: {user.email}</p>
+      <img src={user.photoURL} alt="" />
+     </div>}
     </div>
   );
 }
